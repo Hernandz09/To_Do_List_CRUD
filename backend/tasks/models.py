adb4a0f43c41fe2864138acbf57b04b4d75a4fbe
Yasumy Pastor <yasumy@example.com>
2025-09-07 23:57:25 -0500

    chore: agregar ayuda en el campo title del modelo Task

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, help_text="Escribe el título de la tarea")
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
